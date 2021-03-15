43f401a834267c55af6208145e3fba2e92466c95
Levon Voskaniangit <suxoy1991@gmail.com>
2021-03-15 22:35:32 +0400

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    let stringN = n.toString();
+
+    let reversedString = '';
+    for (let i = stringN.length - 1; i >= 0; i--) {
+        reversedString += stringN[i];
+    }
+
+    return parseInt(reversedString);
 }
